0e02c90b9896e7994495ca2649f50731f76e0e54
VimalAnnamalai <iamvimalannamalai@gmail.com>
2024-10-10 04:06:31 +0530

    Added new problems

--- practice_problems/find_occurence_of_ip.py
+++ practice_problems/find_occurence_of_ip.py
@@ -6,3 +6,11 @@ logs = [
     "10.0.0.2 - - [10/Oct/2020:13:59:36] GET /home.html 200",
     "10.0.0.2 - - [10/Oct/2020:14:00:36] GET /about.html 200"
 ]
+
+import re
+from collections import Counter
+
+results = re.findall(r'\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s', str(logs))
+counter_results = Counter(results)
+
+print(counter_results.most_common()[0])
